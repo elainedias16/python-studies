9d3a48bf6ec162d24a2d8c8f873b3f6b7a82c401
elainedias16 <elainediaspires16@gmail.com>
2022-04-21 16:05:47 -0300

    exercicio 2

---
+++ exerc2.py
@@ -0,0 +1,12 @@
+#Author: Elaine Dias Pires
+
+name = input('What\'s your name?\n')
+age = input('How old are you?\n')
+
+#print('Nice to meet you', name)
+print('Nice to meet you {}' .format(name))
+#print('I am', age,'years old too!')
+print('I am {} years old too!' .format(age))
+
+
+
